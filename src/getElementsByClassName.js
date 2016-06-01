e6c23ae64c214c2feb7caeae73fae7938c4da6b4
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-06-02 00:48:37 +0400

    check the body if it includes the class name and include it

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,21 +6,23 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className,node){
 	var nodes = [];
-	// if (node === undefined) {
+	if (node === undefined) {
 		node = document.body;
-	// }	
-	for (var i = 0; i < node.childNodes.length ; i++){
-		if (typeof node.childNodes[i].classList === 'object'){
-				for (var j=0; j<node.childNodes[i].classList.length; j++) {
-					if(node.childNodes[i].classList[j] === className){
-						nodes.push(node.childNodes[i]);
-						break;
-					}
-				}
+	}
+	if (typeof node.classList === 'object'){
+		for (i=0; i<node.classList.length; i++){
+			if (node.classList[i]===className){
+				nodes.push(node);
+				break;
+			}
 		}
-		// if (node.childNodes[i].childNodes.length > 0) {
-			// getElementsByClassName (className, node.childNodes[i]);
-		// }
+	}
+	for (var i = 0; i < node.childNodes.length ; i++){
+		if (typeof node.childNodes[i] === 'object'){
+			if (node.childNodes[i].childNodes.length > 0) {
+				getElementsByClassName (className, node.childNodes[i]);
+			}
+		}
 	}
 
 return nodes;
