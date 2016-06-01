80b88fed01454bec2f75e8b122cfca735781fbbe
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-06-01 16:55:53 +0400

    return same output as JSON.stringfy only for stringfyable values

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,13 +2,38 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
-if (Array.isArray(obj)){
-	return "[" + "]";
-}
-if(typeof obj === 'string'){
-	return '"' + obj + '"';;
-}
- return "" + obj;
+	
+
+
+	var str = ""
+	if (Array.isArray(obj)){
+		str = str + '['
+		for (var i = 0; i<obj.length; i++) {
+				str = str + stringifyJSON(obj[i])
+				if (i !== obj.length-1){
+					str = str + ","
+				}
+			}
+		str = str + ']';
+	} else if (typeof obj === "object" && obj!==null) {
+		str = str + '{';
+		var counter = 0;
+		
+			for (var key in obj) {
+				 if (counter > 0){
+					str = str + ","
+				}
+				counter ++; 
+					str = str + stringifyJSON (key) + ":" + stringifyJSON (obj[key]);
+			}
+		str = str + '}';
+	}
+	else if(typeof obj === 'string'){
+		str = str + '"' + obj + '"';;
+	}  
+	else {
+		str = str + obj;
+	}
+ return str;
 }
